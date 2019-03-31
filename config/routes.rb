f07c5d925b25668a536230ff00ec8e6f7f90356d
joeyharbert <joey.harbert@gmail.com>
2019-03-31 15:20:25 -0500

    Adds sessions create (log in function)

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     post '/users' => 'users#create'
+    post '/sessions' => 'sessions#create'
   end
 end
