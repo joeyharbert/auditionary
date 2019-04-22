1acb5c723e4a0175eb0f697cb2d9f08c0467d097
joeyharbert <joey.harbert@gmail.com>
2019-04-22 12:06:21 -0500

    Adds some stuff to help with db issues in seeds file for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# add times for all pre-existing time slots without start/end times
+
+# time_slots = TimeSlot.where(start_time = nil)
+# now = Time.now
+# a_day_ago = now - 60 * 60 * 24
+
+# time_slots.each do |ts|
+#   start_time = rand(a_day_ago..now)
+#   ts.start_time = start_time
+#   ts.end_time = start_time + 600
+#   ts.save
+# end
+
+# adds start times/end times for all pre-existing audition_days
+
+# audition_days = AuditionDay.where(start_time = nil)
+
+# audition_days.each do |a|
+#   a.start_time = a.time_slots[0].start_time
+#   a.end_time = a.time_slots[0].last.end_time
+#   a.save
+# end
